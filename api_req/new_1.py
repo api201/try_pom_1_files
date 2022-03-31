1fb3e7425a9af68c784e04c2b1c99f63ec9beeff
api201 <api201ok10@gmail.com>
2022-04-01 02:04:50 +0300

    Update: first and second response with Redirect = True or False + typo (responCe not = responSe ! ! !)

--- api_req/new_1.py
+++ api_req/new_1.py
@@ -2,9 +2,15 @@ import requests
 
 #responce = requests.get("https://playground.learnqa.ru/api/hello")
 #responce = requests.get("https://playground.learnqa.ru/api/get_500")
-#responce = requests.get("https://playground.learnqa.ru/api/something")
-responce = requests.get("https://playground.learnqa.ru/api/get_301", allow_redirects=True)
+#responce = requests.get("https://playground.learnqa.ru/api/something") #ОПЕЧАТКА В responCe, need S ! ! !
+response = requests.get("https://playground.learnqa.ru/api/get_301", allow_redirects=False)
 
-print(responce.status_code)
-print()
-print(responce.text)
+first_response = response.history[0]
+second_response = response
+
+print(first_response.url)
+print(second_response.url)
+
+#print(responce.status_code)
+#print()
+#print(responce.text)
