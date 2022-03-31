e2fb085efe8766773dcf5ac916b90c1662f272aa
api201 <api201ok10@gmail.com>
2022-04-01 01:51:26 +0300

    Update: api - hello / get_500 / something / get_301

--- api_req/new_1.py
+++ api_req/new_1.py
@@ -1,1 +1,10 @@
-#
+import requests
+
+#responce = requests.get("https://playground.learnqa.ru/api/hello")
+#responce = requests.get("https://playground.learnqa.ru/api/get_500")
+#responce = requests.get("https://playground.learnqa.ru/api/something")
+responce = requests.get("https://playground.learnqa.ru/api/get_301", allow_redirects=True)
+
+print(responce.status_code)
+print()
+print(responce.text)
