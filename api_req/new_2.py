600a0c8cff8ee5c39370b10925ff60159041d969
api201 <api201ok10@gmail.com>
2022-04-01 19:15:17 +0300

    Update: Parsing JSON + IF/ELSE

--- api_req/new_2.py
+++ api_req/new_2.py
@@ -2,5 +2,17 @@ import json
 
 string_as_json_format = '{"answer":"Hello, User"}'
 obj = json.loads(string_as_json_format)
-print(obj['answer'])
-#print(obj['answer2']) #negative test
+
+key = "answer2"
+
+if key in obj:
+    print(obj[key])
+else:
+    print(f"Ключа {key} в JSON нет")
+
+# import json
+#
+# string_as_json_format = '{"answer":"Hello, User"}'
+# obj = json.loads(string_as_json_format)
+# print(obj['answer'])
+# #print(obj['answer2']) #negative test
